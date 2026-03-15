583176572e60778890693603f497f822af89dabc
A.T <turing747@gmail.com>
2026-03-15 20:55:27 +0800

    feat: Make the parameters of daily4 game verify semantic. TW2-2224

--- canisters/game-verify/src/daily4.rs
+++ canisters/game-verify/src/daily4.rs
@@ -1,16 +1,49 @@
 use ic_cdk::query;
 
-pub type HexString = String;
-
+#[derive(candid::CandidType, candid::Deserialize)]
+pub struct GenerateDaily4BallRequest {
+  pub seed: [u8; 32],
+  pub ball_range_min: u8,
+  pub ball_range_max: u8,
+}
 
 /// Generate Daily4 balls based on four hex-encoded seeds
 #[query]
-fn generate_daily4_ball(seed1: [u8; 32], min: u8, max: u8) -> u8 {
-  game_algorithms::daily4::generate_ball(seed1, min, max)
+fn generate_daily4_ball(request: GenerateDaily4BallRequest) -> u8 {
+  game_algorithms::daily4::generate_ball(request.seed, request.ball_range_min, request.ball_range_max)
+}
+
+#[derive(candid::CandidType, candid::Deserialize)]
+pub struct GenerateDaily4BallsRequest {
+  pub seed1: [u8; 32],
+  pub seed2: [u8; 32],
+  pub seed3: [u8; 32],
+  pub seed4: [u8; 32],
+  pub ball_range_min: u8,
+  pub ball_range_max: u8,
+}
+
+#[derive(candid::CandidType, candid::Deserialize)]
+pub struct GenerateDaily4BallsResponse {
+  pub ball1: u8,
+  pub ball2: u8,
+  pub ball3: u8,
+  pub ball4: u8,
 }
 
 /// Generate Daily4 balls based on four byte-array seeds
 #[query]
-fn generate_daily4_balls(seed1: [u8; 32], seed2: [u8; 32], seed3: [u8; 32], seed4: [u8; 32], min: u8, max: u8) -> Vec<u8> {
-  game_algorithms::daily4::generate_daily4_balls(seed1, seed2, seed3, seed4, min, max)
+fn generate_daily4_balls(request: GenerateDaily4BallsRequest) -> GenerateDaily4BallsResponse {
+  let balls = game_algorithms::daily4::generate_daily4_balls(
+    request.seed1,
+    request.seed2,
+    request.seed3,
+    request.seed4,
+    request.ball_range_min,
+    request.ball_range_max,
+  );
+  let [ball1, ball2, ball3, ball4]: [u8; 4] = balls
+    .try_into()
+    .expect("Expected exactly 4 balls");
+  GenerateDaily4BallsResponse { ball1, ball2, ball3, ball4 }
 }
